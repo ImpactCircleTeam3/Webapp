3793e956169e4938df8557b4b225b697626ff24a
JangasCodingplace <janisgoesser92@gmail.com>
2021-11-28 22:56:58 +0100

    create raw project template

---
+++ apps/find_similar_hashtags/__init__.py
@@ -0,0 +1,5 @@
+from flask import render_template
+
+
+def view():
+    return render_template("find_similar_hashtags/index.html")
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+from flask import Flask
+from apps.find_similar_hashtags import view as find_similar_hashtags_view
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(
+    __name__,
+    static_folder='static/',
+    template_folder='templates/'
+)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.route("/hashtag-finder")
+def hashtag_finder():
+    return find_similar_hashtags_view()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
